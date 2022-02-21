b060583c39e50e202916b69d5c3550a369c4ca67
keg21 <keg21@aber.ac.uk>
2022-02-21 17:08:59 +0000

    Skeleton Classes

--- src/main/java/uk/aber/ac/keg21/musicapp/Song.java
+++ src/main/java/uk/aber/ac/keg21/musicapp/Song.java
@@ -1,4 +1,37 @@
 package uk.aber.ac.keg21.musicapp;
 
 public class Song {
+    private int songID;
+    private String name;
+    private int albumID;
+
+    public Song(int songID, String name, int albumID) {
+        this.songID = songID;
+        this.name = name;
+        this.albumID = albumID;
+    }
+
+    public int getSongID() {
+        return songID;
+    }
+
+    public void setSongID(int songID) {
+        this.songID = songID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAlbumID() {
+        return albumID;
+    }
+
+    public void setAlbumID(int albumID) {
+        this.albumID = albumID;
+    }
 }
